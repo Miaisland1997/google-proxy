e34935466edaecfce731513dd7c3efb8ce23eced
Miaisland1997 <793410584@qq.com>
2025-11-27 01:03:09 +0800

    Delete api/index.js

--- api/index.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(request, response) {
-  console.log('Health check called');
-  return response.status(200).json({ 
-    status: 'OK', 
-    message: 'Google Proxy is working!',
-    path: request.url
-  });
-}
